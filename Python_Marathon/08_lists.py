ad75843f0696aea929dca5801d49f5a66e34a4c4
jihyunglee <jhlee7125@gmail.com>
2023-02-24 13:47:19 +0900

    step 8 - lists

---
+++ Python_Marathon/08_lists.py
@@ -0,0 +1,14 @@
+
+friends = ['Mike', 'John', 'Tom', 'Karen', 'Jim']
+
+print(friends) # ['Mike', 'John', 'Tom', 'Karen', 'Jim']
+print(friends[0]) # Mike
+print(friends[1]) # John
+print(friends[2]) # Tom
+print(friends[-2]) # Karen
+print(friends[-1]) # Jim
+print(friends[3:]) # ['Karen', 'Jim']
+print(friends[1:3]) # ['John', 'Tom']
+
+friends[0] = 'Kevin'
+print(friends[0]) # Kevin
