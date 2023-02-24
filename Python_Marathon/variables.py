9168afb71b67bead067fa0666a1b421582c0085d
jihyunglee <jhlee7125@gmail.com>
2023-02-24 12:55:56 +0900

    step 2 - variables & data types: basic data types string, number, boolean

--- Python_Marathon/variables.py
+++ Python_Marathon/variables.py
@@ -1,7 +1,10 @@
-character_name = 'George'
-character_age = '70'
+character_name = 'George' # string
+character_age = 50 # number
+is_male = True # boolean
 
 print("There once was a man named " + character_name + ", ")
 print("he was " + character_age +  " years old. ")
+
+character_name = 'Mike'
 print("He really liked the name " + character_name + ", ")
 print("but didn't like being " + character_age + ".");
