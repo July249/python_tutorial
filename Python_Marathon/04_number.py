0b35907cbf98feee085338c41410444475762a2f
jihyunglee <jhlee7125@gmail.com>
2023-02-24 13:19:27 +0900

    step 4 - Working with Numbers: a bunch of type calculation and methods to calculate numbers

---
+++ Python_Marathon/04_number.py
@@ -0,0 +1,32 @@
+my_num = 5
+
+print(2) # 2
+print(2.0987) # 2.0987
+print(-2.0987) # -2.0987
+print(3 + 4) # 7
+print(3 + 4.5) # 7.5
+print(3 * 4) # 12
+print(3 * 4 + 5) # 17
+print(3 * (4 + 5)) # 27
+print(10 / 3) # 3.3333333333333335
+print(10 % 3) # 1 
+print(10 // 3) # 3
+print(my_num) # 5
+print(str(my_num)) # 5
+# print(my_num + ' my favorite number') # TypeError
+print(str(my_num) + ' my favorite number') # 5 my favorite number
+
+your_num = -5
+print(abs(your_num)) # 5
+# print(pow(2)) # TypeError
+print(pow(3, 2)) # 9
+print(max(4, 6)) # 6
+print(min(4, 6)) # 4
+print(round(4.4)) # 4
+print(round(4.6)) # 5
+
+from math import *
+
+print(floor(3.7)) # 3
+print(ceil(3.7)) # 4
+print(sqrt(36)) # 6
