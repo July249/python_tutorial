3f3b4ec5ff930dbee93fbb516067271db657a718
jihyunglee <jhlee7125@gmail.com>
2023-02-24 13:32:08 +0900

    step 6 - Building a Basic Calculator: (1) input value from user is automatically converted string type, (2) the difference between int and float

---
+++ Python_Marathon/06_basic_calculator.py
@@ -0,0 +1,10 @@
+
+num1 = input('Enter a number: ') # the variable which is getting from user has defaultly string type! 
+num2 = input('Enter another number: ')
+result1 = num1 + num2
+result2 = int(num1) + int(num2)
+result3 = float(num1) + float(num2)
+
+print(result1) # 24
+print(result2) # 6
+print(result3) # 7.5
