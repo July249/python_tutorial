91df68d727c33aebf9d1b32b8cb23fdaa6555482
jihyunglee <jhlee7125@gmail.com>
2023-02-26 02:13:52 +0900

    step 19 - for statement

---
+++ Python_Marathon/19_for.py
@@ -0,0 +1,30 @@
+# arr = []
+# for letter in 'Giraffe Academy':
+#     print(letter)
+#     arr.append(letter)
+
+# print(arr)
+
+# friends = ['Jim', 'Karen', 'Mike']
+# for friend in friends:
+#     print(friend)
+
+# for i in range(10):
+#     print(i) # 0 ~ 9
+
+# for i in range(0, 10):
+#     print(i) # 0 ~ 9
+
+# for i in range(0, 10, 2):
+#     print(i) # 0, 2, 4, 6, 8
+
+# friends = ['Jim', 'Karen', 'Mike']
+# print(len(friends))
+# for i in range(len(friends)):
+#     print(friends[i])
+
+for i in range(5):
+    if i == 0:
+        print('first iteration')
+    else:
+        print('Not first')
