57287d3af99d75de69018749b3cb563d298ce9b7
jihyunglee <jhlee7125@gmail.com>
2023-02-25 23:51:18 +0900

    step 11 - functoins

---
+++ Python_Marathon/11_functions.py
@@ -0,0 +1,15 @@
+def sayhi():
+    print('Hello, User!')
+
+print('top')
+# executed sayhi function
+sayhi()
+print('bottom')
+
+print('===============')
+
+def callme(name):
+    print('Who is it?')
+    print('It\'s ' + name + '!')
+
+callme('Mike')
