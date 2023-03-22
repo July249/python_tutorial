509f789762e05ab5e6c14673fe6c96a1ce5b49a5
jihyunglee <jhlee7125@gmail.com>
2023-03-23 01:21:39 +0900

    step 21 - 2D lists and nested loops

---
+++ Python_Marathon/21_2d_lists_and_nested_loops.py
@@ -0,0 +1,17 @@
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+# print(number_grid[0])
+# print(number_grid[1])
+# print(number_grid[1][2])
+# print(number_grid[2])
+# print(number_grid[3])
+
+for row in number_grid:
+    # print(row)
+    for col in row:
+        print(col)
